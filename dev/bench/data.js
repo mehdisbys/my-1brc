e5cb9c0f324557d37b6c6d46ecf6751c2ca2f59c
github-action-benchmark <github@users.noreply.github.com>
2024-03-14 13:18:20 +0000

    add My 1BRC Benchmar (go) benchmark result for 831dd6ec90d335a809ad8e5a09509c3d82a19b5f

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1710422300157,
+  "repoUrl": "https://github.com/mehdisbys/my-1brc",
+  "entries": {
+    "My 1BRC Benchmar": [
+      {
+        "commit": {
+          "author": {
+            "email": "mehdi.souihed@gmail.com",
+            "name": "Mehdi",
+            "username": "mehdisbys"
+          },
+          "committer": {
+            "email": "mehdi.souihed@gmail.com",
+            "name": "Mehdi",
+            "username": "mehdisbys"
+          },
+          "distinct": true,
+          "id": "831dd6ec90d335a809ad8e5a09509c3d82a19b5f",
+          "message": "Edit workflow output chart",
+          "timestamp": "2024-03-14T13:17:27Z",
+          "tree_id": "df03952094a43b4c59eee9f913c86d11b317aad8",
+          "url": "https://github.com/mehdisbys/my-1brc/commit/831dd6ec90d335a809ad8e5a09509c3d82a19b5f"
+        },
+        "date": 1710422299840,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkBRC",
+            "value": 4024806,
+            "unit": "ns/op",
+            "extra": "280 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
